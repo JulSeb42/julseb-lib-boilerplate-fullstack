105f1a39e6f991d256524f8c296d46e375dd146b
JulSeb42 <julien.sebag@me.com>
2025-04-05 19:32:49 +0200

    Seed users

--- seed/seed.ts
+++ seed/seed.ts
@@ -5,11 +5,11 @@ import mongoose from "mongoose"
 import bcrypt from "bcryptjs"
 import { faker } from "@faker-js/faker"
 import {
-    getRandomString,
-    getRandomAvatar,
-    convertToEmail,
-    generateNumbers,
-    getRandom,
+	getRandomString,
+	getRandomAvatar,
+	convertToEmail,
+	generateNumbers,
+	getRandom,
 } from "@julseb-lib/utils"
 import { UserModel } from "../server/models/User.model"
 import type { User } from "../shared/types"
@@ -20,52 +20,51 @@ const salt = bcrypt.genSaltSync()
 const hash = bcrypt.hashSync(password, salt)
 
 const MONGODB_URI =
-    process.env.MONGODB_URI ||
-    "mongodb://localhost/julseb-lib-boilerplate-fullstack"
+	"mongodb+srv://julien:AmazingPassword42@cluster0.lyb91.mongodb.net/boilerplate-react-rest"
 
 mongoose.connect(MONGODB_URI)
 
 const realUser: Partial<User> = {
-    fullName: "Julien Sebag",
-    email: "julien@email.com",
-    password: hash,
-    verified: true,
-    verifyToken: getRandomString(20),
-    role: "admin",
+	fullName: "Julien Sebag",
+	email: "julien@email.com",
+	password: hash,
+	verified: true,
+	verifyToken: getRandomString(20),
+	role: "admin",
 }
 
 const fakeUsers: Array<Partial<User>> = generateNumbers(0, 98).map(() => {
-    const genders = getRandom(["male", "female"])
-    const fullName = faker.person
-        .fullName(genders as any)
-        .replaceAll("PhD", "")
-        .replaceAll("DVM", "")
-        .replaceAll("DDS", "")
-        .replaceAll("Mr. ", "")
-        .replaceAll("MD", "")
-        .replaceAll("Jr.", "")
+	const genders = getRandom(["male", "female"])
+	const fullName = faker.person
+		.fullName(genders as any)
+		.replaceAll("PhD", "")
+		.replaceAll("DVM", "")
+		.replaceAll("DDS", "")
+		.replaceAll("Mr. ", "")
+		.replaceAll("MD", "")
+		.replaceAll("Jr.", "")
 
-    return {
-        fullName,
-        email: convertToEmail(fullName),
-        password: hash,
-        verified: true,
-        verifyToken: getRandomString(20),
-        avatar: getRandomAvatar(genders as any),
-        role: "user",
-    }
+	return {
+		fullName,
+		email: convertToEmail(fullName),
+		password: hash,
+		verified: true,
+		verifyToken: getRandomString(20),
+		avatar: getRandomAvatar(genders as any),
+		role: "user",
+	}
 })
 
 UserModel.insertMany([realUser, ...fakeUsers])
-    .then(users => {
-        console.log(
-            `Success, you added ${users.length} user${
-                users.length > 1 ? "s" : ""
-            } to the db`
-        )
-        mongoose.connection.close()
-    })
-    .catch(err => console.log(err))
+	.then(users => {
+		console.log(
+			`Success, you added ${users.length} user${
+				users.length > 1 ? "s" : ""
+			} to the db`
+		)
+		mongoose.connection.close()
+	})
+	.catch(err => console.log(err))
 
 // Run `yarn tsx seed/seed.ts` from root folder
 // or `yarn seed-users`
