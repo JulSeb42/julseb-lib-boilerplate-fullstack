98b129f22fc847a47b01543a64c9217263920efc
Julien <julien.sebag@me.com>
2025-05-06 13:42:12 +0200

    Clean

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,17 +1,9 @@
-import { useEffect } from "react"
 import { Text } from "@julseb-lib/react"
 import { useAuthContext } from "context"
 import { Page } from "components"
-import { userService } from "api"
 
 export const Homepage = () => {
 	const { isLoggedIn, user } = useAuthContext()
-
-	console.log({ isLoggedIn, user })
-
-	useEffect(() => {
-		userService.allUsers().then(res => console.log({ res }))
-	}, [])
 
 	return (
 		<Page title="Home">
